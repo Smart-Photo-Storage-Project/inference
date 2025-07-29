bf8c61b9762c65a152cd2780db328c6c633e5dc1
Rikza Kurnia Almujtaba Lubis <rikza.kurnia@ui.ac.id>
2025-07-29 14:40:47 +0700

    feat: receive metadata for image embedding

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 # app/main.py
 
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, UploadFile, File, HTTPException, Form
 from app.models import TextEmbeddingRequest
 from app.embedding_service import get_image_embedding, get_text_embedding
 import numpy as np
@@ -8,11 +8,22 @@ import numpy as np
 app = FastAPI()
 
 @app.post("/embed/image")
-async def embed_image(file: UploadFile = File(...)):
+async def embed_image(
+    file: UploadFile = File(...),
+    name: str = Form(...),
+    user_id: str = Form(...),
+    upload_at: int = Form(...),):
     try:
         contents = await file.read()
         embedding = get_image_embedding(contents)
-        return {"embedding": embedding.tolist()}
+
+        return {
+            "embedding": embedding.tolist(),
+            "name": name,
+            "user_id": user_id,
+            "upload_at": upload_at,
+        }
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
